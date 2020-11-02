8b8d5b136bdf88aa14f7cea200e634db58ab6b7d
Andreas Krutzler <wi19b007@technikum-wien.at>
2020-11-02 18:48:40 +0100

    Gherkin und step definition erstellt

---
+++ Taschenrechner/SpecFlowFeature1.feature
@@ -0,0 +1,20 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to calculate trigg. functions
+
+@mytag
+Scenario: Sinus berechenen
+	Given the number is 3.14159
+	When the sinus function is used
+	Then the result should be 0
+
+Scenario: Cosinus berechenen
+	Given the number is 3.14159
+	When the sinus function is used
+	Then the result should be -1
+
+Scenario: Tangens berechenen
+	Given the number is 3.14159
+	When the sinus function is used
+	Then the result should be 0
---
+++ Taschenrechner/SpecFlowFeature1Steps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Taschenrechner
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [Given(@"the number is (.*)")]
+        public void GivenTheNumberIs(Decimal p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the sinus function is used")]
+        public void WhenTheSinusFunctionIsUsed()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
